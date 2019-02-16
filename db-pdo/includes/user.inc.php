58cc410965fc9b1394a301431dfb23799b58dcc2
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-02-16 19:55:54 +0600

    Add PHP PDO

---
+++ db-pdo/includes/user.inc.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+class User  extends Dbh{
+
+    public function getAllUser() {
+        $stmt = $this->connect()->query("SELECT * FROM MYUSER");
+
+        while($row = $stmt->fetch()) {
+            $uid = $row['uid'];
+            return $uid;
+
+        }
+        
+
+    }
+
+
+    public function getUserWithCountCheck() {
+        $id = 1;
+        $uid = "Utshaw";
+
+        $stmt = $this->connect()->prepare("SELECT * FROM MYUSER WHERE id = ? AND uid = ?");
+        $stmt->execute([$id, $uid]);
+
+        if($stmt->rowCount()){ 
+            while($row = $stmt->fetch()) {
+                return $row['uid'];
+            }
+        }else {
+
+        }
+    }
+    
+    
+}
---
+++ db-pdo/index.php
@@ -0,0 +1,26 @@
+
+<?php
+
+include_once 'includes/dbh.inc.php';
+include_once 'includes/user.inc.php';
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // print_r(PDO::getAvailableDrivers());
+
+        $object = new User();
+        echo $object->getUserWithCountCheck();
+
+    ?>
+</body>
+</html>
